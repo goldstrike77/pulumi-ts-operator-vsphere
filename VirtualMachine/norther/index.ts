bd8bf715506deba67048988fdfcad695c6b95a6a
goldstrike <suzhetao@gmail.com>
2022-10-04 22:52:37 +0800

    modified:   VirtualMachine/norther/index.ts 	modified:   VirtualMachine/souther/index.ts

--- VirtualMachine/norther/index.ts
+++ VirtualMachine/norther/index.ts
@@ -9,7 +9,7 @@ const deploy_spec = [
                 cluster: "Cluster",
                 tags: ["norther", "k8s"], // [Project, Group]
                 datastore: "ds_node01",
-                template: "template_Rocky8",
+                template: "template_CentOS7",
                 network: "VM Network",
                 domain: "home.local",
                 gateway: "192.168.0.1",
